a79f7ff7dfcba51f3172ab4244ba81a934e7e523
deniskrivosheev <denis.krivosheev@autodesk.com>
2015-07-08 14:49:04 +0500

    feat(reader): added backend requests for /api/text

--- client/app/reader/reader.controller.js
+++ client/app/reader/reader.controller.js
@@ -1,8 +1,15 @@
 'use strict';
 
 angular.module('evolutionApp')
-  .controller('ReaderCtrl', function ($scope, $modal) {
-
+  .controller('ReaderCtrl', function ($scope, $modal, $http, $log) {
+    $scope.texts = [];
+    $http.get('/api/text')
+      .success(function(textsList){
+        angular.extend($scope.text, textsList);
+      })
+      .error(function(data, status){
+        $log.error('Can not get text list. Server response with status: ', status, 'And data: ', data);
+      });
 
     $scope.addText = function(){
       $modal.open({
@@ -11,7 +18,14 @@ angular.module('evolutionApp')
         controller: 'AddTextModalCtrl',
         size: 'lg'
       }).result.then(function(textObj){
-          console.log(textObj);
+          $http.post('/api/text', textObj)
+            .success(function(createdText){
+              $scope.texts.push(createdText);
+              $log.info('Text has been successfully created')
+            })
+            .error(function(data, status){
+              $log.error('Can not create text. Server response with status: ', status, 'And data: ', data);
+            })
         });
     }
   });
